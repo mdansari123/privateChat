c1df6348866f97974ff1a75de2b9d6fcdcab8deb
mdansari123 <saddamansari52@hotmail.com>
2017-09-01 23:37:39 +0530

    final

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ var io = require('socket.io').listen(app.listen(port));
 require('./config')(app, io);
 require('./routes')(app, io);
 
-console.log('Your application is running on http://localhost:' + port);
+console.log('Your application is running on https://localhost:' + port);
